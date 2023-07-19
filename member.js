72aa0e6301a970d7ed231f867d6b5dd75dffa79b
Yuan Ren <36680944+picarro-yren@users.noreply.github.com>
2023-07-19 02:58:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = new Member();
+    member.name = "John";
+    member.age = 30;
+    member.skills = ["C#", "ASP.NET", "MVC"];
+    member.showSkills();
+}
